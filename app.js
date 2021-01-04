8f53556d71839a0a905d3b28d717ef2bb8f3a6c2
Helen <paznachka@gmail.com>
2021-01-04 15:23:32 +0200

    added mobile-menu

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+
+    // mobile-menu
+
+    let mobileMenu = document.querySelector(".nav-mobile-menu");
+    let mainMenu = document.querySelector(".navigation");
+
+    mobileMenu.addEventListener("click", function() {
+        mobileMenu.classList.toggle("active-menu");
+        if(mobileMenu.classList.contains("active-menu")) {
+            mainMenu.classList.add("active-menu");
+            document.body.style.overflow = "hidden";
+        } else {
+            mainMenu.classList.remove("active-menu");
+            document.body.style.overflow = "auto";
+        }
+    })
+
+    // slider
    
     $('.your-class').slick({
         dots:true,
